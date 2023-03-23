74c5af68d379d8f19a62e7f9853782877115c8bd
Liamka <liamka@me.com>
2023-03-23 20:39:45 +0300

    Fixes

--- examples/Powershell/Invoke-WebRequest.ps1
+++
@@ -1,3 +0,0 @@
-$headers=@{}
-$headers.Add("X-ApiTube-Key", "***KEY***")
-$response = Invoke-WebRequest -Uri 'https://apitube.io/v1/sets/***ID_HERE***?limit=250&offset=0' -Method GET -Headers $headers
